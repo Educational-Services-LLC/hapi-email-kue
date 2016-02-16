440d4abad838d6f37dc0e31255b70784bce70bb1
spruce-bruce <aaron@synapsestudios.com>
2016-02-15 17:28:31 -0700

    mandrill driver eslint to hapi standards :(

--- src/drivers/mandrill.js
+++ src/drivers/mandrill.js
@@ -1,18 +1,30 @@
-var _        = require('lodash');
-var mandrill = require('mandrill-api');
+'use strict';
 
-module.exports = function(options) {
-    var mandrillClient = new mandrill.Mandrill(options.key);
+const _ = require('lodash');
+const Mandrill = require('mandrill-api');
+
+module.exports = function (options) {
+
+    const mandrillClient = new Mandrill.Mandrill(options.key);
 
     return {
-        send : function(emailObject) {
-            var driver = this;
-            return new Promise(function(resolve, reject) {
-                if (!emailObject.from) reject('no from address provided');
-                if (!emailObject.subject) reject('no subject provided');
-                if (!emailObject.template) reject('no template provided');
+        send : function (emailObject) {
 
-                var sendObj = {
+            return new Promise((resolve, reject) => {
+
+                if (!emailObject.from) {
+                    reject('no from address provided');
+                }
+
+                if (!emailObject.subject) {
+                    reject('no subject provided');
+                }
+
+                if (!emailObject.template){
+                    reject('no template provided');
+                }
+
+                const sendObj = {
                     template_name    : emailObject.template,
                     template_content : [],
                     async            : false,
@@ -21,13 +33,11 @@ module.exports = function(options) {
                         subject    : emailObject.subject,
                         from_email : typeof emailObject.from === 'string' ? emailObject.from : emailObject.from.email,
                         from_name  : emailObject.from.name,
-                        to         : driver._formatTo(emailObject.to, reject),
+                        to         : this._formatTo(emailObject.to, reject),
                         headers    : {
                             'reply-to' : typeof emailObject.from === 'string' ? emailObject.from : emailObject.from.email
                         },
-                        global_merge_vars : _.map(emailObject.templateVars, function(value, key) {
-                            return {name : key, content : value};
-                        })
+                        global_merge_vars : _.map(emailObject.templateVars, (value, key) => ({ name : key, content : value }))
                     }
                 };
 
@@ -35,62 +45,63 @@ module.exports = function(options) {
                     sendObj.message.from_name = emailObject.from.name;
                 }
 
-                mandrillClient.messages.sendTemplate(sendObj, function(result) {
-                    resolve(result);
-                }, function(err) {
-                    reject('Mandrill error - ' + err.name + ': ' + err.message);
-                });
+                mandrillClient.messages.sendTemplate(
+                    sendObj,
+                    (result) => resolve(result),
+                    (err) => reject('Mandrill error - ' + err.name + ': ' + err.message)
+                );
             });
         },
 
-        _formatTo : function(to, reject) {
-            var driver = this;
+        _formatTo : function (to, reject) {
 
             // If there's no whitelist, then always send to the trap address if it's defined
-            to = (! options.whitelist || ! options.whitelist.length) ? options.trap || to : to;
+            to = (!options.whitelist || !options.whitelist.length) ? options.trap || to : to;
 
             if (typeof to === 'string') {
                 return [{
-                    email : driver._checkWhitelist(to, reject),
+                    email : this._checkWhitelist(to, reject),
                     type  : 'to'
                 }];
-            } else if (typeof to === 'object') {
-                var formatObject = function(to) {
-                    if (! to.email) reject('failure to provide a recipeient\'s email');
-                    to.email = driver._checkWhitelist(to.email, reject);
-                    to.type = 'to';
-                    return to;
+            }
+            else if (typeof to === 'object') {
+                const formatObject = function (recipient) {
+
+                    if (!recipient.email) {
+                        reject('failure to provide a recipeient\'s email');
+                    }
+
+                    recipient.email = this._checkWhitelist(emailAddressObject.email, reject);
+                    recipient.type = 'to';
+                    return recipient;
                 };
 
                 if (Object.prototype.toString.call(to) !== '[object Array]') {
                     return [formatObject(to)];
-                } else {
-                    return _.map(to, function(recipient) {
-                        return formatObject(recipient);
-                    });
                 }
+
+                return _.map(to, (recipient) => formatObject(recipient));
             }
         },
 
-        _checkWhitelist : function(emailAddress, reject) {
-            if (! options.whitelist || ! options.whitelist.length) {
+        _checkWhitelist : function (emailAddress, reject) {
+
+            if (!options.whitelist || !options.whitelist.length) {
                 return emailAddress;
             }
 
-            var domainPattern = /@(.*)$/,
-                domain = emailAddress.match(domainPattern)[1];
+            const domainPattern = /@(.*)$/;
+            const domain = emailAddress.match(domainPattern)[1];
 
-            if (_.findIndex(
-                options.whitelist,
-                function(whitelisted) { return domain === whitelisted;}) >= 0
-            ) {
+            if (_.findIndex(options.whitelist, (whitelisted) => domain === whitelisted) >= 0) {
                 return emailAddress;
-            } else {
-                if (! options.trap) {
-                    reject('trap option must be set if using whitelist');
-                } else {
-                    return options.trap;
-                }
+            }
+
+            if (!options.trap) {
+                reject('trap option must be set if using whitelist');
+            }
+            else {
+                return options.trap;
             }
         }
     };
